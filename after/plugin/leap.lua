5d32cb709b032500afa595fe7a28e02fce384ff4
Alexander Ng <alexng353@gmail.com>
2024-01-26 21:34:02 -0800

    don't remap global maps lol

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -1,4 +1,4 @@
 vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)', { silent = true, desc = 'Leap forward' })
 vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)', { silent = true, desc = 'Leap backward' })
-vim.keymap.set({ 'n', 'x', 'o' }, 'ggs', '<Plug>(leap-from-window)',
+vim.keymap.set({ 'n', 'x', 'o' }, 'gls', '<Plug>(leap-from-window)',
   { silent = true, desc = 'Leap from window (global leap)' })

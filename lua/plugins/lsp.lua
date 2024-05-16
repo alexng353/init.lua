faba21e0591a00900f4d139bf34911418aca8cf3
Alexander Ng <alexng353@gmail.com>
2024-05-16 15:36:01 -0700

    add rebelot/terminal.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,11 +8,11 @@ return {
           package_pending = "➜",
           package_uninstalled = "✗"
         }
-      }
+      },
     }
   },
   { 'williamboman/mason-lspconfig.nvim' },
-  { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
   { 'neovim/nvim-lspconfig' },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/nvim-cmp' },

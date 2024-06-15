e2d128c33521a1f86904187b42bb74d3d6df9108
Alexander Ng <alexng353@gmail.com>
2024-06-15 08:07:00 -0700

    updates

--- lua/alexng353/remaps.lua
+++ lua/alexng353/remaps.lua
@@ -54,3 +54,34 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- Function to surround visual selection with brackets or quotes
+SurroundSelection = function (start_char, end_char)
+  -- Get the current visual selection
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
+
+  -- Get the lines in the visual selection
+  local lines = vim.fn.getline(start_pos[2], end_pos[2])
+
+  -- If the selection is on a single line, adjust the start and end columns
+  if #lines == 1 then
+    lines[1] = lines[1]:sub(1, start_pos[3] - 1) .. start_char .. lines[1]:sub(start_pos[3], end_pos[3]) .. end_char .. lines[1]:sub(end_pos[3] + 1)
+  else
+    -- For multi-line selection, add start_char and end_char to the first and last lines
+    lines[1] = lines[1]:sub(1, start_pos[3] - 1) .. start_char .. lines[1]:sub(start_pos[3])
+    lines[#lines] = lines[#lines]:sub(1, end_pos[3]) .. end_char .. lines[#lines]:sub(end_pos[3] + 1)
+  end
+
+  -- Set the modified lines back to the buffer
+  vim.fn.setline(start_pos[2], lines)
+
+  -- Exit visual mode
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), 'n', true)
+end
+
+-- Key mappings for visual mode
+vim.api.nvim_set_keymap('v', '<leader>[', ':lua SurroundSelection("[", "]")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>\'', ':lua SurroundSelection("\'", "\'")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>"', ':lua SurroundSelection("\\\"", "\\\"")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>(', ':lua SurroundSelection("(", ")")<CR>', { noremap = true, silent = true })

b9d9a9487d5eb4daa9dab7af2af1808d80b2116e
Alexander Ng <alexng353@gmail.com>
2024-06-15 08:10:04 -0700

    disable ibl rainbows

--- after/plugin/ibl.lua
+++ after/plugin/ibl.lua
@@ -21,19 +21,19 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-vim.g.rainbow_delimiters = { highlight = highlight }
+-- vim.g.rainbow_delimiters = { highlight = highlight }
 -- require("ibl").setup { indent = { highlight = highlight } }
 require("ibl").setup({
   scope = {
-    show_start = true,
-    show_end = true,
+    show_start = false,
+    show_end = false,
   },
   indent = {
-    highlight = highlight,
+    -- highlight = highlight,
   }
 })
 
-hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+-- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
 -- local gid = vim.api.nvim_create_augroup("indent_blankline", { clear = true })
 -- vim.api.nvim_create_autocmd("InsertEnter", {
